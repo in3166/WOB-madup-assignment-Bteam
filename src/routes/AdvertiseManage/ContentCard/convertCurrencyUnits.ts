258db6acda797507795286b423a74f9b9a6b2a91
in3166 <in3166@naver.com>
2022-05-25 14:00:38 +0900

    refactor: convertCurrencyUnits

--- src/routes/AdvertiseManage/ContentCard/convertCurrencyUnits.ts
+++ src/routes/AdvertiseManage/ContentCard/convertCurrencyUnits.ts
@@ -19,26 +19,27 @@ export const convertCurrencyUnits = (number: number) => {
   const unitWords = ['', '만', '억', '조']
   const splitUnit = 10000
   const splitCount = unitWords.length
-  const resultArray = []
+  const resultArray: number[] = []
   let resultString = ''
   let chon
   let won
 
-  for (let i = 0; i < splitCount; i += 1) {
-    let unitResult = (inputNumber % splitUnit ** (i + 1)) / splitUnit ** i
-    // console.log(unitResult)
+  unitWords.forEach((value, index) => {
+    let unitResult = (inputNumber % splitUnit ** (index + 1)) / splitUnit ** index
     unitResult = Math.floor(unitResult)
-    if (i === 0) {
+    if (index === 0) {
       won = unitResult % 1000 > 0 ? `${unitResult % 1000}원` : ''
       chon = Math.floor(unitResult / 1000) > 0 ? `${Math.floor(unitResult / 1000)}천` : ''
     } else if (unitResult > 0) {
-      resultArray[i] = unitResult
+      resultArray[index] = unitResult
     }
-  }
+  })
+
+  resultArray.forEach((value, index) => {
+    if (resultArray[index]) resultString = String(numberFormat(resultArray[index])) + unitWords[index] + resultString
+  })
+
   const a = `${chon}${won}`
-  for (let i = 0; i < resultArray.length; i += 1) {
-    if (resultArray[i]) resultString = String(numberFormat(resultArray[i])) + unitWords[i] + resultString
-  }
 
   return resultString + a
 }
